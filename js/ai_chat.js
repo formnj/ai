40bcecc3fb5f6875f00042f70c20079c5f3b7ebe
jhlee <jhlee@rianag.com>
2024-02-28 18:01:40 +0900

    resize test

--- js/ai_chat.js
+++ js/ai_chat.js
@@ -12,7 +12,9 @@ const ai_babysuni = {
     }, keyword_select: function(){
         event.target.classList.add('active');
     }, scroll: function(){ //채팅 입력 시 스크롤 하단으로 이동
-        $('.ai_chat_cont').scrollTop($('.ai_chat_cont_inner')[0].scrollHeight);
+        $('.ai_chat_cont').animate({
+            scrollTop : $('.ai_chat_cont_inner')[0].scrollHeight
+        }, 1000);
     }
 }
 
@@ -115,70 +117,3 @@ var comment_action = `<ul class="comment_action">
     <!-- //tooltip -->
 </li>
 </ul>`;
-
-
-/* drag resize */
-const resizer = document.getElementById('dragMe');
-const leftSide = resizer.previousElementSibling;
-const rightSide = resizer.nextElementSibling;
-
-// 마우스의 위치값 저장을 위해 선언
-let x = 0;
-let y = 0;
-
-// 크기 조절시 왼쪽 Element를 기준으로 삼기 위해 선언
-let leftWidth = 0;
-
-// resizer에 마우스 이벤트가 발생하면 실행하는 Handler
-const mouseDownHandler = function (e) {
-    // 마우스 위치값을 가져와 x, y에 할당
-    x = e.clientX;
-    y = e.clientY;
-    // left Element에 Viewport 상 width 값을 가져와 넣음
-    leftWidth = leftSide.getBoundingClientRect().width;
-
-    // 마우스 이동과 해제 이벤트를 등록
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-const mouseMoveHandler = function (e) {
-    // 마우스가 움직이면 기존 초기 마우스 위치에서 현재 위치값과의 차이를 계산
-    const dx = e.clientX - x;
-    const dy = e.clientY - y;
-
-  	// 크기 조절 중 마우스 커서를 변경함
-    // class="resizer"에 적용하면 위치가 변경되면서 커서가 해제되기 때문에 body에 적용
-    document.body.style.cursor = 'col-resize';
-    
-    // 이동 중 양쪽 영역(왼쪽, 오른쪽)에서 마우스 이벤트와 텍스트 선택을 방지하기 위해 추가
-    leftSide.style.userSelect = 'none';
-    leftSide.style.pointerEvents = 'none';
-    
-    rightSide.style.userSelect = 'none';
-    rightSide.style.pointerEvents = 'none';
-    
-    // 초기 width 값과 마우스 드래그 거리를 더한 뒤 상위요소(container)의 너비를 이용해 퍼센티지를 구함
-    // 계산된 퍼센티지는 새롭게 left의 width로 적용
-    const newLeftWidth = ((leftWidth + dx) * 100) / resizer.parentNode.getBoundingClientRect().width;
-    leftSide.style.width = `${newLeftWidth}%`;
-};
-
-const mouseUpHandler = function () {
-    // 모든 커서 관련 사항은 마우스 이동이 끝나면 제거됨
-    resizer.style.removeProperty('cursor');
-    document.body.style.removeProperty('cursor');
-
-    leftSide.style.removeProperty('user-select');
-    leftSide.style.removeProperty('pointer-events');
-
-    rightSide.style.removeProperty('user-select');
-    rightSide.style.removeProperty('pointer-events');
-
-    // 등록한 마우스 이벤트를 제거
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-};
-
-// 마우스 down 이벤트를 등록
-resizer.addEventListener('mousedown', mouseDownHandler);
